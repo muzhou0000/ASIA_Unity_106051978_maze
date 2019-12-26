551d93c046f82961d4bd97b57f025590cad1bab7
muzhou0000 <55180633+muzhou0000@users.noreply.github.com>
2019-12-26 22:34:16 +0800

    動畫設置

---
+++ Unity_106051978_maze/Assets/Script/Cameraconl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cameraconl : MonoBehaviour
+{
+
+    public Transform cam, mouse;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = Vector3.Lerp(cam.position, mouse.position, 0.5f * Time.deltaTime);
+        cam.position = pos;
+    }
+}
---
+++ Unity_106051978_maze/Assets/Script/Mousemove.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mousemove : MonoBehaviour
+{
+    #region 數據
+    [Header("速度"),Range(50,2000)]
+    public float speed;
+    [Header("旋轉"),Range(50, 200)]
+    public float turn;
+    public Rigidbody rig;
+    public Transform Tra;
+    public Animator ani;
+    #endregion
+
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        Walk();
+        Run();
+        Attack();
+    }
+
+    #region 行動
+
+    void Walk()
+    {
+        float v = Input.GetAxis("Vertical");
+        rig.AddForce(Tra.forward * v * speed * Time.deltaTime);
+        ani.SetBool("walk", v!=0); 
+    }
+    void Run()
+    {
+        float h = Input.GetAxis("Horizontal");
+        Tra.Rotate(0,turn * h * Time.deltaTime,0);
+
+    }
+    void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ani.SetBool("Attack",true);
+
+        }
+        else
+        {
+            ani.SetBool("Attack", false);
+
+        }
+
+    }
+
+    #endregion
+}
